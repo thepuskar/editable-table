5cec6a807b0ae5b35bb75e01f8ebe435ac7c84dd
Puskar Adhikari <puskar.ad@gmail.com>
2025-04-12 01:20:37 +0545

    feat: implement button component with variant and size support

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { EditableTable } from "@/components/editable-table";
+
+function App() {
+  return (
+    <>
+      <div className="container px-8 py-8">
+        <EditableTable />
+      </div>
+    </>
+  );
+}
+
+export default App;
