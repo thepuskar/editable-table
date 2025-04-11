47470d889046dad6c6e7423860ec0647c46bdc79
Puskar Adhikari <puskar.ad@gmail.com>
2025-04-12 01:42:46 +0545

    feat: integrate VirtualizedCombobox with dynamic options and selection handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import { EditableTable } from "@/components/editable-table";
+import { VirtualizedCombobox } from "@/components/ui/virtualized-combobox";
+import { useState } from "react";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState("item 3");
+  function generateItems() {
+    const items: string[] = [];
+    for (let i = 1; i <= 20000; i++) {
+      items.push(`item ${i}`);
+    }
+    return items;
+  }
+
+  const initialOptions: string[] = generateItems();
+
+  const onSelectHandler = (val: unknown) => {
+    console.log("Selected Value", val);
+    setSelectedOption(val as string);
+  };
   return (
     <>
       <div className="container px-8 py-8">
         <EditableTable />
+        <div className="mt-4">
+          <VirtualizedCombobox
+            options={initialOptions}
+            value={selectedOption}
+            onSelect={onSelectHandler}
+          />
+        </div>
       </div>
     </>
   );
